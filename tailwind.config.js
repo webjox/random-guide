8603c6a8c3eabf68611085a2e398efc3da84ee8c
deniskorbakov <korbakovd@gmail.com>
2023-10-21 01:29:23 +0300

    dev: add configs params

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,29 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    blocklist: [
+        'bg-white',
+    ],
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
     ],
 
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'main' : '#4E2982',
+            'header' : '#976BD6',
+            'footer' : '#320571',
+
+        },
+
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
@@ -18,5 +33,9 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('tailwind-scrollbar-hide'),
+        require('flowbite/plugin'),
+    ],
 };
