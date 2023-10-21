db27f9919985f6c8516ba82a318ac310de5b7bc9
Vsevolod <vs.girenko@mail.ru>
2023-10-21 16:06:38 +0300

    dev: layout fixes

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,10 +4,9 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white overflow-hidden"
-        >
+    <div class="min-h-screen flex flex-col sm:justify-center items-stretch pt-6 sm:pt-0 bg-gray-100">
+        <h1 class="text-sm font-bold text-gray-600 text-center mt-6">HyperCleverFace</h1>
+        <div class="w-full sm:max-w-md px-6 overflow-hidden">
             <slot />
         </div>
     </div>
