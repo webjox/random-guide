2e324496d664c1fb03f3f3f63a234b41ca95746f
Vsevolod <vs.girenko@mail.ru>
2023-10-21 03:48:12 +0300

    feat: random place page template

---
+++ resources/js/Pages/RandomPlace.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Map from '@/Components/Map.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { Button, Rating, Heading } from 'flowbite-vue';
+
+const place = {
+    title: 'Random place',
+    description: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    `,
+    coordinates: {
+        lat: 0,
+        lon: 0,
+    },
+    rating: 3,
+};
+</script>
+
+<template>
+    <Head title="Random Place" />
+    <GuestLayout class="grid place-items-center">
+        <div class="grid place-items-center">
+            <Rating class="mx-auto my-2" :rating="place.rating" />
+        </div>
+
+        <section class="flex flex-col gap-2">
+            <Heading tag="h1" class="my-2 text-2xl font-semibold text-gray-700 text-center">{{ place.title }}</Heading>
+            <p class="text-sm text-gray-500 italic text-center my-2">{{ place.description }}</p>
+        </section>
+
+        <Map />
+        <div class="flex gap-6 justify-around mt-6">
+            <Button color="dark" outline>Won't go</Button>
+            <Button color="green" outline>You got me!</Button>
+        </div>
+    </GuestLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RandomPlaceController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -34,5 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/random', RandomPlaceController::class)->name('random');
 
 require __DIR__.'/auth.php';
