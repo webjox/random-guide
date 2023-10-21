4c16488f5f67b07bb92b4a3bd2e1972be18bb629
Vsevolod <vs.girenko@mail.ru>
2023-10-21 20:24:40 +0300

    fix: import duplicate

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\PlaceListController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RandomPlaceController;
-use App\Http\Controllers\PlaceListController;
-use App\Http\Controllers\RandomPlaceController;
-use App\Http\Controllers\MapApiController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
