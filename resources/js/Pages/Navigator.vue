e204d4edbfee5238b233f21d07522637e94be7aa
Vsevolod Girenko <69035880+sauromates@users.noreply.github.com>
2023-10-22 11:44:53 +0300

    feat: passing accepted place (#9)

---
+++ resources/js/Pages/Navigator.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Layout from '@/Layouts/Layout.vue';
+import { Head } from '@inertiajs/vue3';
+import Map from '@/Components/Map.vue';
+
+defineProps({
+    place: Object
+})
+
+</script>
+
+<template>
+    <Layout>
+        <Head title="Navigator" />
+        <h1 class="text-2xl">
+            {{ `Идем в ${place.name}` }}
+        </h1>
+        <Map :coordinates="[place.longitude, place.latitude]" />
+    </Layout>
+</template>
