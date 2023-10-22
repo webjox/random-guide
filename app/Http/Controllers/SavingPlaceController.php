a5fbf19d8eec6fcd87a76c95b3143484591ab2f1
de-lolec <matvey20158@mail.ru>
2023-10-22 13:55:50 +0300

    dev: auth user

--- app/Http/Controllers/SavingPlaceController.php
+++ app/Http/Controllers/SavingPlaceController.php
@@ -14,13 +14,20 @@ class SavingPlaceController extends Controller
     public function __invoke(Request $request): Response
     {
         $response = $request->input('response');
-        $userVkId = $request->input('userVkId');
         $is_confirmed = $request->boolean('is_confirmed');
 
-        $user = User::firstOrCreate([
-            'vk_id' => $userVkId,
-            'rating' => 0,
-        ]);
+        if (!auth()->check()) {
+      
+            $user = new User;
+            $user->vk_id = 0;
+            $user->save();
+        
+         
+            auth()->login($user);
+        }
+    
+        
+        auth()->login($user);
 
         $addressJson = json_encode($response['address_details']);
 
